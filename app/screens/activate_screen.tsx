ad97bb552cb924c01b41b01f47727ee7201b4815
dymok <dymokilan@gmail.com>
2025-01-06 22:58:43 +0100

    layout changes + added temporary big validation until backend api is done

--- app/screens/activate_screen.tsx
+++ app/screens/activate_screen.tsx
@@ -1,10 +1,33 @@
-import { Button, SizableText, YStack, Image, Input } from 'tamagui';
+import React, { useState } from 'react';
+import { Button, SizableText, YStack, Image, Input, XStack } from 'tamagui';
 import { Dimensions } from 'react-native';
 import color from '../../constants/Colors';
+import { Toast, useToastController } from '@tamagui/toast'; // Import for toast
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 
-export default function ActivateScreen( {navigation}  ) {
+function ActivateDevice(bigNumber, navigation, showErrorToast) {
+    // TODO: Replace this with API request to validate the BIG number
+    if (bigNumber === '1234') {
+        navigation.navigate('StartShiftScreen');
+    } else {
+        // Show error toast when BIG number is invalid
+        showErrorToast();
+    }
+}
+
+export default function ActivateScreen({ navigation }) {
+    const [bigNumber, setBigNumber] = useState(''); // State to manage input value
+    const toast = useToastController(); // Toast controller for error messages
+
+    // Function to show a custom error toast
+    const showErrorToast = () => {
+        toast.show('Invalid BIG number', {
+            message: "The BIG number you entered is not valid.",
+            native: false, // Using custom toast style
+        });
+    };
+
     return (
         <YStack
             f={1}
@@ -15,56 +38,75 @@ export default function ActivateScreen( {navigation}  ) {
         >
             <YStack
                 bg="#E1F4F6"
-                width={(screenWidth * 85) / 100}
-                height={(screenHeight * 80) / 100}
+                width={(screenWidth * 80) / 100}
+                height={(screenHeight * 75) / 100}
                 borderRadius="$10"
                 elevation="$0.25"
-                padding="$4"
+                p="$6"
+                ai="center"
+                jc="space-between"
             >
-                {/* Image */}
-                <Image
-                    src={require('../../assets/images/skilled-Hospice-Nurse-768x612.png')}
-                    width="80%"
-                    resizeMode="contain"
-                    mx="auto"
-                    mt="$4" // Add margin-top to position the image
-                />
-
-                {/* Input */}
-                <YStack f={1} ai="center" jc="flex-end" mt="$6">
-                    <Input
-                        placeholder="Voer BIG-nummer in"
-                        width="70%"
-                        height="$5"
-                        borderRadius="$8"
-                        bg="white"
-                        borderColor="#CCCCCC"
-                        borderWidth={1}
-                        px="$4"
-                        fontSize={16}
+                {/* Image Section */}
+                <YStack ai="center" jc="center" mt="$4">
+                    <Image
+                        src={require('../../assets/images/skilled-Hospice-Nurse-768x612.png')}
+                        style={{
+                            width: '60%',
+                            aspectRatio: 4 / 3,
+                            maxHeight: (screenHeight * 27) / 100,
+                        }}
+                        resizeMode="contain"
+                        mx="auto"
                     />
                 </YStack>
 
-                {/* Button */}
-                <YStack ai="center" jc="flex-end" mt="$6">
+                {/* Input Section */}
+                <YStack f={0} ai="center" jc="center" width="100%" mt="$4">
+                    <XStack
+                        position="relative"
+                        width="100%"
+                        height="$6"
+                        bg="white"
+                        borderRadius="$8"
+                        borderColor="#CCCCCC"
+                        borderWidth={1}
+                        px="$4"
+                        ai="center"
+                        jc="space-between"
+                    >
+                        <Input
+                            placeholder="Voer BIG-nummer in"
+                            fontSize={16}
+                            f={1}
+                            bg="transparent"
+                            borderWidth={0}
+                            value={bigNumber} // Bind input value to state
+                            onChangeText={(text) => setBigNumber(text)} // Update state on change
+                        />
+                    </XStack>
+                </YStack>
+
+                {/* Button Section */}
+                <YStack ai="center" jc="center" mt="$6" mb="$4">
                     <Button
                         bg={color.light.accent}
                         borderRadius="$10"
-                        width="$16"
+                        width={(screenWidth * 50) / 100}
                         height="$6"
-                        mb="$10"
-                        mx="auto"
                         pressStyle={{
                             bg: color.light.accent_focus,
                         }}
-                        onPress={() => navigation.navigate('StartShiftScreen')}
+                        onPress={() => ActivateDevice(bigNumber, navigation, showErrorToast)} // Pass input value and showErrorToast to function
                     >
-                        <SizableText col={color.light.accent_content} size="$6">
+                        <SizableText col={color.light.accent_content} size="$5" textAlign="center">
                             Activeer Apparaat
                         </SizableText>
                     </Button>
                 </YStack>
             </YStack>
+
+            {/* Toast for error message */}
+            <Toast />
         </YStack>
     );
 }
