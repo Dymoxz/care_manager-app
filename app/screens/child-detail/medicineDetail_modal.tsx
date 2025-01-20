8c72f361343346adc07735289bdf94c5721c903f
Mech <stefrensma@gmail.com>
2025-01-20 11:53:04 +0100

    display medicine modal with markdown

--- app/screens/child-detail/medicineDetail_modal.tsx
+++ app/screens/child-detail/medicineDetail_modal.tsx
@@ -1,18 +1,40 @@
-import {Button, Dialog, SizableText, Unspaced,} from 'tamagui';
+import { Button, Dialog, SizableText, Unspaced, YStack, View, ScrollView } from 'tamagui';
 import React from "react";
-import {X} from "@tamagui/lucide-icons";
+import { X } from "@tamagui/lucide-icons";
+import Markdown from 'react-native-markdown-display';
 
+interface DosageGoal {
+    dosageGoal: string;
+    ingestType: {
+        ingestType: string;
+        issuingType: string[];
+        ageAndWeight: {ageAndWeight: string; description: string;}[];
+    }[]
+}
+
+interface Medicine {
+    _id: string;
+    name: string;
+    brandName: string;
+    atcCode: string;
+    dosageGoals: DosageGoal[];
+}
 
 interface MedicineDetailModalProps {
     visible: boolean;
     onClose: () => void;
     screenWidth: number;
-    medicine: { name: string } | null; // Allow null
+    medicine: Medicine | null; // Allow null
 }
 
 export default function MedicineDetailModal({visible, onClose, screenWidth, medicine}: MedicineDetailModalProps) {
-    // Use fallbackMedicine if medicine is null
-    const fallbackMedicine = {name: 'Medicine'};
+    const fallbackMedicine: Medicine = {
+        _id: '',
+        name: 'Medicijn',
+        brandName: 'N/A',
+        atcCode: 'N/A',
+        dosageGoals: [],
+    };
     const medicineToDisplay = medicine || fallbackMedicine;
 
     return (
@@ -27,22 +49,79 @@ export default function MedicineDetailModal({visible, onClose, screenWidth, medi
                 />
                 <Dialog.Content
                     elevate
-                    animation={['quick', {opacity: {overshootClamping: true}}]}
-                    enterStyle={{y: -20, opacity: 0, scale: 0.9}}
-                    exitStyle={{y: 10, opacity: 0, scale: 0.95}}
+                    animation={['quick', {opacity: { overshootClamping: true } }]}
+                    enterStyle={{ y: -20, opacity: 0, scale: 0.9 }}
+                    exitStyle={{ y: 10, opacity: 0, scale: 0.95 }}
                     gap="$4"
+                    marginVertical={screenWidth * 0.1}
                     padding="$3"
                     width={screenWidth * 0.9}
-                    bg='$container'
-                    borderRadius='$8'
-                    style={{maxWidth: screenWidth * 0.9}}
+                    bg="$container"
+                    borderRadius="$8"
+                    style={{ maxWidth: screenWidth * 0.9 }}
                 >
-                    <Dialog.Title fontSize='$7' mt="$5" marginHorizontal="$2" textAlign='center'>
-                        {medicineToDisplay.name} {/* Use the name from medicineToDisplay */}
-                    </Dialog.Title>
-                    <SizableText textAlign='center' col="gray">
-                        Medijn informatie
-                    </SizableText>
+                    <ScrollView>
+                        <Dialog.Title fontSize='$7' mt="$5" marginHorizontal="$2" textAlign='center'>
+                            {medicineToDisplay.name}
+                        </Dialog.Title>
+                        <SizableText textAlign='center' col="gray" mb="$2">
+                            Medijn informatie
+                        </SizableText>
+                        <YStack width="100%" paddingHorizontal="$2">
+                            <SizableText size="$5" fontWeight="bold" color="$text" mt="$2">
+                                Merknaam:
+                            </SizableText>
+                            <SizableText size="$5"  color="$text" >
+                                {medicineToDisplay.brandName}
+                            </SizableText>
+                            <SizableText size="$5" fontWeight="bold" color="$text" mt="$2">
+                                ATC Code:
+                            </SizableText>
+                            <SizableText size="$5"  color="$text" >
+                                {medicineToDisplay.atcCode}
+                            </SizableText>
+
+                            {medicineToDisplay.dosageGoals.map((dosageGoal, index) => (
+                                <YStack key={index} mt="$2" mb="$3" width="100%">
+                                    <SizableText size="$5" fontWeight="bold" color="$text">
+                                        Dosering advies:
+                                    </SizableText>
+                                    <SizableText size="$5" color="$text">
+                                        {dosageGoal.dosageGoal}
+                                    </SizableText>
+                                    {dosageGoal.ingestType.map((ingestType, typeIndex) => (
+                                        <YStack  key={typeIndex} ml="$2">
+                                            <SizableText  size="$5" fontWeight="bold" color="$text" mt="$1">
+                                                Inname type:
+                                            </SizableText>
+                                            <SizableText  size="$5"  color="$text">
+                                                {ingestType.ingestType}
+                                            </SizableText>
+                                            {ingestType.ageAndWeight && ingestType.ageAndWeight.map((ageWeight, ageIndex) => (
+                                                <YStack key={ageIndex}  ml="$2">
+                                                    <SizableText  size="$5" fontWeight="bold" color="$text"  mt="$1">
+                                                        Leeftijd en Gewicht:
+                                                    </SizableText>
+                                                    <SizableText  size="$5" color="$text" >
+                                                        {ageWeight.ageAndWeight}
+                                                    </SizableText>
+                                                    <View  ml="$2">
+                                                        <Markdown
+                                                            style={{fontSize: 16, fontWeight: 'normal'}}
+
+                                                        >
+                                                            {ageWeight.description}
+                                                        </Markdown>
+                                                    </View>
+                                                </YStack>
+
+                                            ))}
+                                        </YStack>
+                                    ))}
+                                </YStack>
+                            ))}
+                        </YStack>
+                    </ScrollView>
                     <Unspaced>
                         <Dialog.Close asChild>
                             <Button
